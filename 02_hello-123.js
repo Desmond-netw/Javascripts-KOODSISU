e4b3466ee3bedf60036ed55e72a049ebb8e1ccd8
Desmond-netw <dsquare200@gmail.com>
2026-04-20 21:58:45 +0300

    ouputing 123

---
+++ 02_hello-123.js
@@ -0,0 +1,9 @@
+/* 
+Functional requirements
+Create a JavaScript file which outputs 123 on the standard output.
+
+console.log() can print more than just strings.
+*/
+
+// Solution
+console.log(123);
