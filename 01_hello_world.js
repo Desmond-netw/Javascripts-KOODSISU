7212c121850cdb5300c3cec6b19990d640f4d840
Desmond-netw <dsquare200@gmail.com>
2026-04-20 21:56:38 +0300

    initial hello-world

---
+++ 01_hello_world.js
@@ -0,0 +1,15 @@
+/*
+Functional requirements
+A "Hello, World!" is a short program, used to demonstrate the basic syntax of a programming language. It's often the first thing attempted when learning a new language or framework.
+
+It's time for you to write your first piece of JavaScript code.
+
+When you run your file with Node.js, it should print Hello, world! to the console.
+
+
+*/
+
+
+
+// Solution
+console.log("Hello, world!");
