314e7afb8cff6881b849aa65cb7bfba72d50cfa8
Desmond-netw <dsquare200@gmail.com>
2026-05-17 21:03:22 +0300

    shallow and deep copy in js

--- 09_shallow_and_deep.js
+++ 09_shallow_and_deep.js
@@ -14,3 +14,23 @@ const Person =  {
     zipCode: "10001",
   },
 }
+
+//  shallow copy of the person object 
+// 1. using the spread operator to create a shallow copy of the person object.
+// shallow copy means the new copy will have same properties of the original object.
+const shallowCopy = {...Person};
+
+// deep copy of the person object
+//  using JSON.parse and JSON.stringify to create a deep copy.
+
+const deepCopy = JSON.parse(JSON.stringify(Person));
+
+// step two: modify the original object
+Person.name = "Emmanuel Odame";
+Person.hobbies.push("fishing");
+Person.address.street = "00400 Independence Avenue";
+
+// step three: log the variables
+console.log("Original Person:", Person);
+console.log("Shallow copy:", shallowCopy);
+console.log("Deep copy:", deepCopy);
