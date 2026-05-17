7036536c85d8f93532482976e6b1158ce66fcf1d
Desmond-netw <dsquare200@gmail.com>
2026-05-17 20:54:48 +0300

    person object initilize

---
+++ 09_shallow_and_deep.js
@@ -0,0 +1,16 @@
+// Create a JavaScript file which does nothing when executed with Node.js. It should contain the following 2 objects:
+
+const Person =  {
+  name: "John Doe",
+  age: 30,
+  city: "New York",
+  hobbies: [
+    "reading",
+    "cooking",
+    "hiking",
+  ],
+  address: {
+    street: "123 Main Street",
+    zipCode: "10001",
+  },
+}
